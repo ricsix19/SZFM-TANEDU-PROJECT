a875266c47d6cf686ca885896efc66d16ae53366
ricsix19 <richardladanyi30@gmail.com>
2025-10-02 09:07:40 +0200

    chore: actually commiting the new hierarchy, forgot git add

---
+++ backend/src/main/java/org/example/tanedu/TanEduApplication.java
@@ -0,0 +1,13 @@
+package org.example.tanedu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TanEduApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TanEduApplication.class, args);
+    }
+
+}
---
+++ backend/src/test/java/org/example/tanedu/TanEduApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.tanedu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TanEduApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
