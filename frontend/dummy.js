db5b03a8cb06f4a3e12c6534cad1988a24e0349a
gulyasjanii <gulyas.janos0410117@gmail.com>
2025-10-26 13:15:37 +0100

    dummy.js a tesztadatokhoz

---
+++ frontend/dummy.js
@@ -0,0 +1,80 @@
+window.dummyFelhasznalok = [
+  {
+    username: "teszt",
+    password: "1234",
+    teljesNev: "Teszt Elek",
+    azonosito: "123456",
+    anyjaNeve: "Teszt Anyja",
+    szuletesHelye: "Budapest",
+    szuletesIdeje: "2000-01-01",
+    telefon: "0612345678",
+    email: "teszt@pelda.hu",
+    kepzesNeve: "Informatika",
+    kezdesIdopontja: "2018-09-01",
+    aktualisFelev: "6",
+    varhatoVegzes: "2023-06-30",
+    orarend: {
+      hetfo: [
+        { targy: "Matematika", tanar: "Kovács Béla", terem: "A101", cim: "Analízis I." },
+        { targy: "Informatika", tanar: "Szabó László", terem: "B202", cim: "Programozás alapjai" }
+      ],
+      kedd: [
+        { targy: "Fizika", tanar: "Tóth Tamás", terem: "C303", cim: "Mechanika" },
+        { targy: "Történelem", tanar: "Kiss Katalin", terem: "D404", cim: "Újkori történelem" }
+      ],
+      szerda: [
+        { targy: "Kémia", tanar: "Varga Júlia", terem: "E105", cim: "Szerves kémia" },
+        { targy: "Biológia", tanar: "Nagy Péter", terem: "F201", cim: "Sejtbiológia" }
+      ],
+      csutortok: [
+        { targy: "Angol", tanar: "Smith Anna", terem: "G101", cim: "Kommunikációs gyakorlat" },
+        { targy: "Földrajz", tanar: "Molnár Gábor", terem: "H303", cim: "Európa földrajza" }
+      ],
+      pentek: [
+        { targy: "Testnevelés", tanar: "Horváth László", terem: "Tornaterem", cim: "Kondicionáló edzés" },
+        { targy: "Rajz", tanar: "Balogh Eszter", terem: "I102", cim: "Alapvető rajztechnikák" }
+      ]
+    },
+    targyak: ["Matematika", "Fizika", "Informatika"],
+    jegyek: { "Matematika": "5", "Fizika": "4", "Informatika": "5" }
+  },
+  {
+    username: "demo",
+    password: "abcd",
+    teljesNev: "Demo Dóra",
+    azonosito: "654321",
+    anyjaNeve: "Demo Anyja",
+    szuletesHelye: "Szeged",
+    szuletesIdeje: "1999-05-15",
+    telefon: "0620123456",
+    email: "demo@pelda.hu",
+    kepzesNeve: "Gazdaságtudomány",
+    kezdesIdopontja: "2017-09-01",
+    aktualisFelev: "8",
+    varhatoVegzes: "2023-06-30",
+    orarend: {
+      hetfo: [
+        { targy: "Közgazdaságtan", tanar: "Török Béla", terem: "A201", cim: "Mikroökonómia" },
+        { targy: "Pénzügy", tanar: "Varga András", terem: "B102", cim: "Vállalati pénzügyek alapjai" }
+      ],
+      kedd: [
+        { targy: "Marketing", tanar: "Kocsis Éva", terem: "C202", cim: "Fogyasztói magatartás" },
+        { targy: "Jog", tanar: "Székely Tamás", terem: "D203", cim: "Gazdasági jog" }
+      ],
+      szerda: [
+        { targy: "Számvitel", tanar: "Bíró Ágnes", terem: "E301", cim: "Vállalati számvitel" },
+        { targy: "Statisztika", tanar: "Fekete Márton", terem: "F204", cim: "Leíró statisztika" }
+      ],
+      csutortok: [
+        { targy: "Vállalati pénzügyek", tanar: "Bálint László", terem: "G305", cim: "Kockázatelemzés" },
+        { targy: "Üzleti kommunikáció", tanar: "Csabai Tímea", terem: "H106", cim: "Tárgyalástechnika" }
+      ],
+      pentek: [
+        { targy: "Projektmenedzsment", tanar: "Király Péter", terem: "I202", cim: "Agilis módszertanok" },
+        { targy: "Gazdasági angol", tanar: "James Susan", terem: "J101", cim: "Üzleti levelezés" }
+      ]
+    },
+    targyak: ["Közgazdaságtan", "Marketing", "Számvitel"],
+    jegyek: { "Közgazdaságtan": "4", "Marketing": "5", "Számvitel": "3" }
+  }
+];
