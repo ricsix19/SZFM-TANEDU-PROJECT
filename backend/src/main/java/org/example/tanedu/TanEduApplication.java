c880d299534d2b7adcf9b47a06e37d963c45b7f4
Kiss Ákos <kissakos2002@mailbox.unideb.hu>
2025-11-04 18:27:55 +0100

    Default department creation and setting admin users department

--- backend/src/main/java/org/example/tanedu/TanEduApplication.java
+++ backend/src/main/java/org/example/tanedu/TanEduApplication.java
@@ -1,8 +1,10 @@
 // java
 package org.example.tanedu;
 
+import org.example.tanedu.Model.Department;
 import org.example.tanedu.Model.Role;
 import org.example.tanedu.Model.User;
+import org.example.tanedu.Repository.DepartmentRepository;
 import org.example.tanedu.Repository.UserRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
@@ -11,6 +13,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 import java.util.Date;
+import java.util.List;
 
 @SpringBootApplication
 public class TanEduApplication {
@@ -20,16 +23,33 @@ public class TanEduApplication {
     }
 
     @Bean
-    CommandLineRunner init(UserRepository userRepository, PasswordEncoder passwordEncoder) {
+    CommandLineRunner init(UserRepository userRepository, PasswordEncoder passwordEncoder,DepartmentRepository departmentRepository) {
         return args -> {
+            List<String> deptNames = List.of("9", "10", "11", "12");
+
+            for (String name : deptNames) {
+                Department d = departmentRepository.findByName(name);
+                if (d == null) {
+                    d = new Department();
+                    d.setName(name);
+                    departmentRepository.save(d);
+                }
+            }
             User foundAdmin = userRepository.findByEmail("tesztadmin@gmail.com");
             if (foundAdmin == null) {
+                Department department = departmentRepository.findByName("9");
+                if (department == null) {
+                    department = new Department();
+                    department.setName("9");
+                    department = departmentRepository.save(department);
+                }
                 User admin = new User();
                 admin.setEmail("tesztadmin@gmail.com");
                 admin.setFirstName("First");
                 admin.setLastName("Admin");
                 admin.setPassword(passwordEncoder.encode("admin123"));
                 admin.setBirthDate(new Date(2002 - 1900, 11, 24));
+                admin.setDepartment(department);
                 admin.setRole(Role.SYSADMIN);
                 userRepository.save(admin);
             } else {
